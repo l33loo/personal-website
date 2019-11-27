4d2567470b82d32afdeba5ceffa538bde9b7cd4b
Lila Karpowicz <karpowicz.lila@gmail.com>
2019-11-26 18:01:40 -0800

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+const hello = "HELLO from webpack!";
+
+console.log(hello);
+
+/* Includes */
+import $ from "jquery";
+import '../../node_modules/jquery-parallax.js/parallax.js';
+
+/* Webpack */
+import '../sass/style.scss';
+// import '../template.html';
+import '../img/hero.jpg';
---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+/*
+TO DO:
+- Implement dev mode
+- Install Babel
+- Add img hashing and optimization, and implement src, href, data-src replacement
+- Install postcss-loader
+*/
+
+const path = require('path');
+const TerserJsPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: "production",
+  entry: ["./src/js/index.js"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: 'js/main.[contenthash].js'
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/style.[contenthash].css'
+    }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Lila Karpowicz, junior front-end web developer',
+      template: 'src/template.html'
+    })
+  ],
+  optimization: {
+    minimizer: [new TerserJsPlugin({}), new OptimizeCssAssetsPlugin({})]
+  },
+  module: {
+    rules: [
+      {
+        test:/\.(sa|sc|c)ss$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: 'img/[name].[ext]' /* TO DO: implement hashing */
+          }
+        }
+      },
+      {
+        test: require.resolve('jquery'),
+        use: [
+          {
+            loader: 'expose-loader',
+            options: 'jQuery'
+          },
+          {
+            loader: 'expose-loader',
+            options: '$'
+          }
+        ]
+      }
+      // {
+      //   test: /\.(html)$/,
+      //   exclude: /node_modules/,
+      //   use:
+      //   {
+      //     loader: "html-loader",
+      //     options: {
+      //       attrs: ["img:src", "link:href", ":data-src"]
+      //     }
+      //   }
+      // }
+    ]
+  },
+  watch: true,
+  watchOptions: {
+    ignored: /node_modules/
+  }
+}
